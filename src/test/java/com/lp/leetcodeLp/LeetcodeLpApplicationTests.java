f09c080780cf3e81fb69f230c26eb494864c665e
longpeng <longpeng@lattebank.com>
2020-04-02 16:50:51 +0800

    add

---
+++ src/test/java/com/lp/leetcodeLp/LeetcodeLpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp.leetcodeLp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeLpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
