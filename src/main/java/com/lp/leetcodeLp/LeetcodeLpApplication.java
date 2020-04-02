49fbf201a6e4204700a8e163570c3065a4e2360d
longpeng <longpeng@lattebank.com>
2020-04-02 16:50:38 +0800

    add

---
+++ src/main/java/com/lp/leetcodeLp/LeetcodeLpApplication.java
@@ -0,0 +1,13 @@
+package com.lp.leetcodeLp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeLpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeLpApplication.class, args);
+	}
+
+}
