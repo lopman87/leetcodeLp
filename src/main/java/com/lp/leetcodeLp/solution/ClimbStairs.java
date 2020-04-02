b8a47c5219ac6314bebee67dbe35067617700abd
longpeng <longpeng@lattebank.com>
2020-04-02 17:02:42 +0800

    add

--- src/main/java/com/lp/leetcodeLp/solution/ClimbStairs.java
+++ src/main/java/com/lp/leetcodeLp/solution/ClimbStairs.java
@@ -1,7 +1,20 @@
 package com.lp.leetcodeLp.solution;
 
 public class ClimbStairs {
-    public int climbStairs(int n) {
-        return 0;
+    public static int climbStairs(int n) {
+        if( n ==1 ){
+            return 1;
+        }
+        if (n ==2 ){
+            return 2;
+        }
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+
+    public static void main(String args[]){
+        long start = System.currentTimeMillis();
+        System.out.println(climbStairs(45));
+        long end = System.currentTimeMillis();
+        System.out.println("cost:"+(end - start) );
     }
 }
