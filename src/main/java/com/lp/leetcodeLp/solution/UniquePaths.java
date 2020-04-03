b353c3c182c2865723f311c273f0b498fbef47cd
longpeng <longpeng@lattebank.com>
2020-04-03 11:16:14 +0800

    add

---
+++ src/main/java/com/lp/leetcodeLp/solution/UniquePaths.java
@@ -0,0 +1,13 @@
+package com.lp.leetcodeLp.solution;
+
+/**
+ * https://leetcode-cn.com/problems/unique-paths/
+ */
+public class UniquePaths {
+
+    public int uniquePaths(int m, int n) {
+
+
+        return 0;
+    }
+}
