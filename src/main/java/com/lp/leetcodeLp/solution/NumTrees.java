297b52c8cc1cc501fed831502f5167d3ba8420b5
lopman <longpeng@lattebank.com>
2020-04-08 23:23:29 +0800

    Add

---
+++ src/main/java/com/lp/leetcodeLp/solution/NumTrees.java
@@ -0,0 +1,17 @@
+package com.lp.leetcodeLp.solution;
+
+/**
+ * https://leetcode-cn.com/problems/unique-binary-search-trees/
+ */
+public class NumTrees {
+    public static int numTrees(int n) {
+
+        return 0;
+    }
+    public static void main(String args[]){
+        long start = System.currentTimeMillis();
+        System.out.println(numTrees(8));
+        long end = System.currentTimeMillis();
+        System.out.println("cost:"+(end - start) );
+    }
+}
