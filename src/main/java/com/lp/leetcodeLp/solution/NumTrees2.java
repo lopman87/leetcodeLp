57e3dd590850d1ec9a8c79e1ab5d1c30b6c8cffd
lopman <longpeng@lattebank.com>
2020-04-11 11:54:15 +0800

    add

--- src/main/java/com/lp/leetcodeLp/solution/NumTrees2.java
+++ src/main/java/com/lp/leetcodeLp/solution/NumTrees2.java
@@ -11,6 +11,38 @@ public class NumTrees2 {
         if (n <= 2){
             return n;
         }
-        return 0;
+        if ( n == 3){
+            return 5;
+        }
+        if ( n == 4){
+            return 14;
+        }
+        return getAns(1, n);
     }
+    private int getAns(int start, int end) {
+        int ans = 0;
+        //此时没有数字，只有一个数字,返回 1
+        if (start >= end) {
+            return 1;
+        }
+        //尝试每个数字作为根节点
+        for (int i = start; i <= end; i++) {
+            //得到所有可能的左子树
+            int leftTreesNum = getAns(start, i - 1);
+            //得到所有可能的右子树
+            int rightTreesNum  = getAns(i + 1, end);
+            //左子树右子树两两组合
+            ans+=leftTreesNum * rightTreesNum;
+        }
+        return ans;
+    }
+
+    public static void main(String args[]){
+        long start = System.currentTimeMillis();
+        NumTrees2 numTrees = new NumTrees2();
+        System.out.println(numTrees.numTrees(4));
+        long end = System.currentTimeMillis();
+        System.out.println("cost:"+(end - start) );
+    }
+
 }
---
+++ src/main/java/com/lp/leetcodeLp/solution/NumTrees3.java
@@ -0,0 +1,24 @@
+package com.lp.leetcodeLp.solution;
+
+public class NumTrees3 {
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+
+    public static void main(String args[]){
+        long start = System.currentTimeMillis();
+        NumTrees3 numTrees = new NumTrees3();
+        System.out.println(numTrees.numTrees(4));
+        long end = System.currentTimeMillis();
+        System.out.println("cost:"+(end - start) );
+    }
+}
