e20801eb122abb7b05fcccceda6a12832d88ed35
lopman <longpeng@lattebank.com>
2020-04-11 13:52:32 +0800

    add

---
+++ src/main/java/com/lp/leetcodeLp/solution/IntegerBreak.java
@@ -0,0 +1,13 @@
+package com.lp.leetcodeLp.solution;
+
+/**
+ * https://leetcode-cn.com/problems/integer-break/
+ */
+public class IntegerBreak {
+
+
+
+    public int integerBreak(int n) {
+        return 0;
+    }
+}
