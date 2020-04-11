db0af070c40af2e1fad2b0180ce5f345b75eb2b6
lopman <longpeng@lattebank.com>
2020-04-11 10:24:29 +0800

    Add

--- src/main/java/com/lp/leetcodeLp/solution/NumTrees.java
+++ src/main/java/com/lp/leetcodeLp/solution/NumTrees.java
@@ -1,7 +1,6 @@
 package com.lp.leetcodeLp.solution;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * https://leetcode-cn.com/problems/unique-binary-search-trees/
@@ -11,8 +10,15 @@ import java.util.Map;
  */
 public class NumTrees {
 
+    public static void main(String args[]){
+        long start = System.currentTimeMillis();
+        NumTrees numTrees = new NumTrees();
+        System.out.println(numTrees.numTrees(4));
+        long end = System.currentTimeMillis();
+        System.out.println("cost:"+(end - start) );
+    }
 
-    public static int numTrees(int n) {
+    public int numTrees(int n) {
         if (n <= 2){
             return n;
         }
@@ -22,13 +28,27 @@ public class NumTrees {
         return doNumTrees(n);
     }
 
-    private static int doNumTrees(int n){
-
-
-        return 0;
+    private int doNumTrees(int n){
+        List<List<Integer>> lists = permute(n);
+        Set<String> tmpSet = new HashSet<>();
+        for (List<Integer> tmpList:lists) {
+            Node root = buildTree(tmpList);
+            StringBuilder stringBuilder = new StringBuilder();
+            visitNode(root,stringBuilder);
+            tmpSet.add(stringBuilder.toString());
+        }
+        return tmpSet.size();
+    }
+    private Node buildTree(List<Integer> tmpList){
+        Node root = new Node(tmpList.get(0));
+        for (int i = 1; i < tmpList.size(); i++) {
+            doBuildTree(root, tmpList.get(i));
+        }
+        return root;
     }
 
-    private void buildTree(Node root, int newVal){
+
+    private void doBuildTree(Node root, int newVal){
         if (root.getCurrentVal() == newVal){
             return;
         }
@@ -37,14 +57,14 @@ public class NumTrees {
                 Node tmp = new Node(newVal);
                 root.setLeftNode(tmp);
             }else{
-                buildTree(root.getLeftNode() , newVal);
+                doBuildTree(root.getLeftNode() , newVal);
             }
         }else{
             if (root.getRightNode() == null){
                 Node tmp = new Node(newVal);
                 root.setRightNode(tmp);
             }else{
-                buildTree(root.getRightNode() , newVal);
+                doBuildTree(root.getRightNode() , newVal);
             }
         }
     }
@@ -103,10 +123,39 @@ public class NumTrees {
     }
 
 
-    public static void main(String args[]){
-        long start = System.currentTimeMillis();
-        System.out.println(numTrees(8));
-        long end = System.currentTimeMillis();
-        System.out.println("cost:"+(end - start) );
+
+
+
+    public List<List<Integer>> permute(int n) {
+        int[] tmp = new int[n];
+        for (int i = 0; i < n; i++) {
+            tmp[i] = i+1;
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        helper(0, tmp, result);
+        return result;
+    }
+
+    private void helper(int start, int[] nums, List<List<Integer>> result){
+        if(start==nums.length-1){
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int num: nums){
+                list.add(num);
+            }
+            result.add(list);
+            return;
+        }
+
+        for(int i=start; i<nums.length; i++){
+            swap(nums, i, start);
+            helper(start+1, nums, result);
+            swap(nums, i, start);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
