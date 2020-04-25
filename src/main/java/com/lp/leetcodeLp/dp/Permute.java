7b2537acb39549227c8a35b875a00d5b45acf8d7
lopman <longpeng@lattebank.com>
2020-04-25 21:42:06 +0800

    Aa

---
+++ src/main/java/com/lp/leetcodeLp/dp/Permute.java
@@ -0,0 +1,43 @@
+package com.lp.leetcodeLp.dp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Permute {
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (nums == null || nums.length ==0){
+            return lists;
+        }
+        if (nums.length==1){
+            List<Integer> list = new ArrayList<>();
+            list.add(nums[0]);
+            lists.add(list);
+            return lists;
+        }
+        Arrays.sort(nums);
+        if (nums.length==2){
+            List<Integer> list1 = new ArrayList<>();
+            list1.add(nums[0]);list1.add(nums[1]);
+            lists.add(list1);
+            List<Integer> list2 = new ArrayList<>();
+            list2.add(nums[1]);list2.add(nums[0]);
+            lists.add(list2);
+            return lists;
+        }
+
+        return lists;
+    }
+
+
+    public static void main(String args[]){
+        long start = System.currentTimeMillis();
+        Permute numTrees = new Permute();
+        int[] p ={1,2,3};
+        System.out.println(numTrees.permute(p));
+        long end = System.currentTimeMillis();
+        System.out.println("cost:"+(end - start) );
+    }
+}
