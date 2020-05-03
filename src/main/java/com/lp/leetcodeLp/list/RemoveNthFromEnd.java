fe47351353e2b330c3d34c5e3ce42846c6808a8f
lopman <longpeng@lattebank.com>
2020-05-03 21:22:37 +0800

    Add

--- src/main/java/com/lp/leetcodeLp/list/RemoveNthFromEnd.java
+++ src/main/java/com/lp/leetcodeLp/list/RemoveNthFromEnd.java
@@ -1,5 +1,9 @@
 package com.lp.leetcodeLp.list;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
  *
@@ -17,23 +21,50 @@ public class RemoveNthFromEnd {
 
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
-
+        if (head == null){
+            return head;
+        }
+        ListNode tmpHead_0 = head;
+        ListNode tmpHead_1 = head;
+        int start_0 = 0;
+        while (tmpHead_0 != null){
+            start_0++;
+            tmpHead_0 = tmpHead_0.next;
+        }
+        int start_1 = start_0 - n;
+        if (start_1 == 0){
+            return head.next;
+        }
+        int flag = 0;
+        while (tmpHead_1 != null){
+            ListNode tmp = null;
+            if (flag+1 == start_1){
+                tmp = tmpHead_1.next;
+                tmpHead_1.next = tmp.next;
+                tmp.next = null;
+                break;
+            }
+            flag++;
+            tmpHead_1 = tmpHead_1.next;
+        }
         return head;
     }
 
     public static void main(String args[]){
         long start = System.currentTimeMillis();
         RemoveNthFromEnd numTrees = new RemoveNthFromEnd();
-        ListNode startNode = new ListNode(0);
         ListNode head = new ListNode(1);
-        startNode.next = head;
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
         head.next.next.next.next.next = new ListNode(6);
-        System.out.println(numTrees.removeNthFromEnd(startNode,2));
+        ListNode lll = numTrees.removeNthFromEnd(head,6);
+        while (lll!=null){
+            System.out.println(lll.val);
+            lll = lll.next;
+        }
         long end = System.currentTimeMillis();
-        System.out.println("cost:"+(end - start) );
+        System.out.println("cost:"+(end - start));
     }
 }
