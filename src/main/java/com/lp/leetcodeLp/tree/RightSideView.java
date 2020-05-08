9a8cb10655f351c4303b361b01e1810fb4ed948b
lopman <longpeng@lattebank.com>
2020-05-09 00:16:29 +0800

    Bj

--- src/main/java/com/lp/leetcodeLp/tree/RightSideView.java
+++ src/main/java/com/lp/leetcodeLp/tree/RightSideView.java
@@ -1,7 +1,9 @@
 package com.lp.leetcodeLp.tree;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
@@ -26,8 +28,25 @@ public class RightSideView {
 
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> integerList = new ArrayList<>();
-
         return integerList;
+    }
+
+    private void levleVisit(TreeNode root){
+        Queue<TreeNode> integerQueue = new ArrayDeque<>();
+        integerQueue.add(root);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (!integerQueue.isEmpty()){
+            TreeNode head = integerQueue.poll();
+            stringBuilder.append(head.val+".");
+            if (head.left != null){
+                integerQueue.add(head.left);
+            }
+            if (head.right != null){
+                integerQueue.add(head.right);
+            }
+
+        }
     }
 
 
@@ -42,7 +61,7 @@ public class RightSideView {
         root.right.left = new TreeNode(6);
         root.right.right = new TreeNode(20);
 
-        numTrees.rightSideView(root);
+        numTrees.levleVisit(root);
         long end = System.currentTimeMillis();
         System.out.println("cost:"+(end - start) );
     }
