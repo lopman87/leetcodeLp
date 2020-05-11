ef85b52e68c485a392cd283013045021db6ac709
lopman <longpeng@lattebank.com>
2020-05-12 00:08:19 +0800

    Sa

--- src/main/java/com/lp/leetcodeLp/dp/MaxValue.java
+++ src/main/java/com/lp/leetcodeLp/dp/MaxValue.java
@@ -28,7 +28,20 @@ package com.lp.leetcodeLp.dp;
 public class MaxValue {
 
     public int maxValue(int[][] grid) {
-        return 0;
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < grid[0].length; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i-1];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[i].length; j++) {
+                dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]) + grid[i][j];
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
     }
 
 
@@ -36,9 +49,8 @@ public class MaxValue {
         long start = System.currentTimeMillis();
         MaxValue numTrees = new MaxValue();
         int[][] A = {
-                        {1,3,1},
-                        {1,5,1},
-                        {4,2,1}
+                        {1,2,5},
+                        {3,2,1}
                     };
         System.out.println(numTrees.maxValue(A));
         long end = System.currentTimeMillis();
