548a71e031e7fe61300c90c67b05067c2c4821fc
lopman <longpeng@lattebank.com>
2020-05-14 22:58:09 +0800

    Zx

---
+++ src/main/java/com/lp/leetcodeLp/dp/FindNumberOfLIS.java
@@ -0,0 +1,46 @@
+package com.lp.leetcodeLp.dp;
+
+/**
+ * 给定一个未排序的整数数组，找到最长递增子序列的个数。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,5,4,7]
+ * 输出: 2
+ * 解释: 有两个最长递增子序列，分别是 [1, 3, 4, 7] 和[1, 3, 5, 7]。
+ * 示例 2:
+ *
+ * 输入: [2,2,2,2,2]
+ * 输出: 5
+ * 解释: 最长递增子序列的长度是1，并且存在5个子序列的长度为1，因此输出5。
+ * 注意: 给定的数组长度不超过 2000 并且结果一定是32位有符号整数。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-longest-increasing-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FindNumberOfLIS {
+
+    public int findNumberOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int res = 0;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i] , dp[j]+1);
+                    res = Math.max(res, dp[i]);
+                }
+            }
+        }
+        return res;
+    }
+
+
+
+    public static void main(String args[]){
+        FindNumberOfLIS numTrees = new FindNumberOfLIS();
+        int[] A = {1,3,5,4,7};
+        System.out.println(numTrees.findNumberOfLIS(A));
+    }
+}
--- src/main/java/com/lp/leetcodeLp/dp/LongestSubsequence.java
+++ src/main/java/com/lp/leetcodeLp/dp/LongestSubsequence.java
@@ -1,5 +1,9 @@
 package com.lp.leetcodeLp.dp;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给你一个整数数组 arr 和一个整数 difference，请你找出 arr 中所有相邻元素之间的差等于给定 difference 的等差子序列，并返回其中最长的等差子序列的长度。
  *
@@ -34,14 +38,36 @@ package com.lp.leetcodeLp.dp;
 public class LongestSubsequence {
 
     public int longestSubsequence(int[] arr, int difference) {
-        return 0;
+
+        int[] dp = new int[arr.length];
+        int res = 0;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] - arr[j] == difference){
+                    dp[i] = Math.max(dp[i] , dp[j] + 1);
+                    res = Math.max(res , dp[i]);
+                }
+            }
+        }
+        return res+1;
+    }
+
+    public int longestSubsequence1(int[] arr, int difference) {
+        int res = 1;
+        Map<Integer, Integer> times = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int temp = times.getOrDefault(arr[i] - difference, 0) + 1;
+            times.put(arr[i], temp);
+            res = Math.max(res ,temp);
+        }
+        return res;
     }
 
     public static void main(String args[]){
         long start = System.currentTimeMillis();
         LongestSubsequence numTrees = new LongestSubsequence();
-        int[] A = {1,2,3,4};
-        System.out.println(numTrees.longestSubsequence(A ,1));
+        int[] A = {1,3,5,7,9};
+        System.out.println(numTrees.longestSubsequence1(A ,2));
         long end = System.currentTimeMillis();
         System.out.println("cost:"+(end - start) );
     }
