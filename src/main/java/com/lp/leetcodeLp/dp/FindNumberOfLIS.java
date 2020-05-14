796f2f8917028f42f4923cffd601388ba1ccd3d3
lopman <longpeng@lattebank.com>
2020-05-15 00:18:29 +0800

    Ds

--- src/main/java/com/lp/leetcodeLp/dp/FindNumberOfLIS.java
+++ src/main/java/com/lp/leetcodeLp/dp/FindNumberOfLIS.java
@@ -1,5 +1,8 @@
 package com.lp.leetcodeLp.dp;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * 给定一个未排序的整数数组，找到最长递增子序列的个数。
  *
@@ -22,10 +25,15 @@ package com.lp.leetcodeLp.dp;
 public class FindNumberOfLIS {
 
     public int findNumberOfLIS(int[] nums) {
+        if (nums == null || nums.length ==0){
+            return 0;
+        }
         int[] dp = new int[nums.length];
-        dp[0] = 1;
-        int res = 0;
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = 1;
+        }
+        int res = 1;
+        for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]){
                     dp[i] = Math.max(dp[i] , dp[j]+1);
@@ -33,14 +41,14 @@ public class FindNumberOfLIS {
                 }
             }
         }
+        System.out.println(Arrays.toString(dp));
         return res;
     }
-
-
+    //最长的上升子序列是 [2,3,7,101]，它的长度是 4
 
     public static void main(String args[]){
         FindNumberOfLIS numTrees = new FindNumberOfLIS();
-        int[] A = {1,3,5,4,7};
-        System.out.println(numTrees.findNumberOfLIS(A));
+        int[] A = {10,9,2,5,3,7,101,18};
+        System.out.println(numTrees.findNumberOfLIS(A ));
     }
 }
