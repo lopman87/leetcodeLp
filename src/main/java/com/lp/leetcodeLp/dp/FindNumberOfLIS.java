d22833d5bc0b77ee6b657d64b6f26b13c4798639
lopman <longpeng@lattebank.com>
2020-06-11 23:31:20 +0800

    接口

--- src/main/java/com/lp/leetcodeLp/dp/FindNumberOfLIS.java
+++ src/main/java/com/lp/leetcodeLp/dp/FindNumberOfLIS.java
@@ -25,23 +25,30 @@ import java.util.stream.Collectors;
 public class FindNumberOfLIS {
 
     public int findNumberOfLIS(int[] nums) {
-        if (nums == null || nums.length ==0){
-            return 0;
-        }
-        int[] dp = new int[nums.length];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = 1;
-        }
-        int res = 1;
-        for (int i = 0; i < nums.length; i++) {
+        if (nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        int[] counter = new int[n];
+        Arrays.fill(dp, 1);
+        Arrays.fill(counter, 1);
+        int max = 0;
+        for (int i = 0;i<n; i++) {
             for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[i] , dp[j]+1);
-                    res = Math.max(res, dp[i]);
+                if (nums[i] > nums[j]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                        counter[i] = counter[j];
+                    } else if (dp[j] + 1 == dp[i]) {
+                        counter[i] += counter[j];
+                    }
                 }
             }
+            max = Math.max(max, dp[i]);
         }
-        System.out.println(Arrays.toString(dp));
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == max) res += counter[i];
+        }
         return res;
     }
 
