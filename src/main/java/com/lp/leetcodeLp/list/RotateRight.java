957b67994666235e2bc4e1925785044dd9853e06
lopman <longpeng@lattebank.com>
2020-08-10 23:28:28 +0800

    Ad

--- src/main/java/com/lp/leetcodeLp/list/RotateRight.java
+++ src/main/java/com/lp/leetcodeLp/list/RotateRight.java
@@ -3,6 +3,20 @@ package com.lp.leetcodeLp.list;
 public class RotateRight {
 
     public ListNode rotateRight(ListNode head, int k) {
+        int length = 0;
+        ListNode tmpHead = head;
+        while (tmpHead != null){
+            length ++;
+            tmpHead = tmpHead.next;
+        }
+        if (length == 0){
+            return head;
+        }
+        int realK = k % length;
+        if (realK == 0){
+            return head;
+        }
+
 
         return null;
     }
