06e5ee6e5c5f94164d2a5b5b483d241b1c49501c
longpeng <longpeng@lattebank.com>
2020-08-11 20:31:43 +0800

    vas

--- src/main/java/com/lp/leetcodeLp/list/RotateRight.java
+++ src/main/java/com/lp/leetcodeLp/list/RotateRight.java
@@ -60,7 +60,7 @@ public class RotateRight {
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
 
-        head.next.next.next.next.next = new ListNode(6);
+        //head.next.next.next.next.next = new ListNode(6);
 
 
         ListNode result = numTrees.rotateRight(head ,2);
