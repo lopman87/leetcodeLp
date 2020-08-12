6933e1dbe02f71cf1a7c4f5dba6a44d7ac8d7b76
longpeng <longpeng@lattebank.com>
2020-08-12 20:52:03 +0800

    vas

--- src/main/java/com/lp/leetcodeLp/array/Merge.java
+++ src/main/java/com/lp/leetcodeLp/array/Merge.java
@@ -57,11 +57,13 @@ public class Merge {
     }
 
     public static void main(String[] args){
-        int[] nums1 = {1,2,3,0,0,0,0};
-        int[] nums2 = {2,5,6};
+        int[] nums1 = {-1,0,0,3,3,3,0,0,0};
+        int[] nums2 = {1,2,2};
         Merge cache = new Merge();
-        cache.merge(nums1 , 3 , nums2 ,3);
+        cache.merge(nums1 , 6 , nums2 ,3);
 
         System.out.println(Arrays.toString(nums1));
+
+        String pre = "[-1,0,0,1,2,2,3,3,3]";
     }
 }
