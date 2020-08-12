8dec9ec5b75b29b34bc513eae62cda244de4d27c
longpeng <longpeng@lattebank.com>
2020-08-12 20:31:06 +0800

    vas

--- src/main/java/com/lp/leetcodeLp/array/Merge.java
+++ src/main/java/com/lp/leetcodeLp/array/Merge.java
@@ -1,6 +1,8 @@
 package com.lp.leetcodeLp.array;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
 
 /**
  * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
@@ -32,14 +34,34 @@ public class Merge {
             int element2 = nums2[i];
             nums1[nums1.length -1 -i] = element2;
         }
-        Arrays.sort(nums1);
+
+        Comparator<Integer> employeeNameComparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer e1, Integer e2) {
+                if (e1 != 0 && e2 != 0) {
+                    return e1.compareTo(e2);
+                } else {
+                    return e1 == 0 ? 1 : -1;
+                }
+            }
+        };
+
+        Integer[] ever = IntStream.of( nums1 ).boxed().toArray( Integer[]::new );
+
+        Arrays.sort(ever, employeeNameComparator);
+
+        for (int i = 0; i < ever.length; i++) {
+            nums1[i] = ever[i];
+        }
+
     }
 
     public static void main(String[] args){
-        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums1 = {1,2,3,0,0,0,0};
         int[] nums2 = {2,5,6};
         Merge cache = new Merge();
         cache.merge(nums1 , 3 , nums2 ,3);
+
         System.out.println(Arrays.toString(nums1));
     }
 }
