4af25111c6fc042f9fe3d39c3f530e8e3002848e
longpeng <longpeng@lattebank.com>
2020-08-14 15:48:47 +0800

    5654

--- src/main/java/com/lp/leetcodeLp/design/MyHashSet.java
+++ src/main/java/com/lp/leetcodeLp/design/MyHashSet.java
@@ -1,5 +1,7 @@
 package com.lp.leetcodeLp.design;
 
+import java.util.Arrays;
+
 /**
  * 不使用任何内建的哈希表库设计一个哈希集合
  *
@@ -24,8 +26,18 @@ package com.lp.leetcodeLp.design;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/design-hashset
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ * int[] a = {1, 2, 3};
+ *     // make a one bigger
+ *     a = Arrays.copyOf(a, a.length + 1);
+ *     for (int i : a)
+ *         System.out.println(i);
  */
 public class MyHashSet {
+
+    int[] array = {};
+
 
     /** Initialize your data structure here. */
     public MyHashSet() {
@@ -33,26 +45,48 @@ public class MyHashSet {
     }
 
     public void add(int key) {
-
+        for (int a:array) {
+            if (a == key){
+                return;
+            }
+        }
+        array = Arrays.copyOf(array, array.length + 1);
+        array[array.length - 1] = key;
     }
 
     public void remove(int key) {
-
+        for (int i =0 ; i < array.length ; i++) {
+            if (array[i] == key){
+                remove(key, i);
+                return;
+            }
+        }
     }
+
+    private void remove(int key , int index){
+        for (int i = index; i < array.length-1; i++) {
+            array[i] = array[i+1];
+        }
+        array = Arrays.copyOf(array, array.length - 1);
+    }
+
 
     /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
+        for (int i =0 ; i < array.length ; i++) {
+            if (array[i] == key){
+                return true;
+            }
+        }
         return false;
     }
 
 
     public static void main(String[] args){
         MyHashSet queue = new MyHashSet();
-        queue.add(1);
-        queue.remove(2);
+        queue.add(2);
         System.out.println(queue.contains(2));;  // 返回 1
-        System.out.println(queue.contains(3));   // 返回 1
-        System.out.println(queue.contains(4)); // 返回 false
-
+        queue.remove(2);
+        System.out.println(queue.contains(2));   // 返回 1
     }
 }
