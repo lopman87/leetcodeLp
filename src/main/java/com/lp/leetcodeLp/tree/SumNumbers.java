a17c59d1ac37b0ded3ea8ee1ca0cdce80a21f669
longpeng <longpeng@lattebank.com>
2020-08-17 20:47:24 +0800

    asv

--- src/main/java/com/lp/leetcodeLp/tree/SumNumbers.java
+++ src/main/java/com/lp/leetcodeLp/tree/SumNumbers.java
@@ -1,45 +1,108 @@
 package com.lp.leetcodeLp.tree;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
+/**
+ * 给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+ *
+ * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+ *
+ * 计算从根到叶子节点生成的所有数字之和。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3]
+ *     1
+ *    / \
+ *   2   3
+ * 输出: 25
+ * 解释:
+ * 从根到叶子节点路径 1->2 代表数字 12.
+ * 从根到叶子节点路径 1->3 代表数字 13.
+ * 因此，数字总和 = 12 + 13 = 25.
+ * 示例 2:
+ *
+ * 输入: [4,9,0,5,1]
+ *     4
+ *    / \
+ *   9   0
+ *  / \
+ * 5   1
+ * 输出: 1026
+ * 解释:
+ * 从根到叶子节点路径 4->9->5 代表数字 495.
+ * 从根到叶子节点路径 4->9->1 代表数字 491.
+ * 从根到叶子节点路径 4->0 代表数字 40.
+ * 因此，数字总和 = 495 + 491 + 40 = 1026.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-root-to-leaf-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class SumNumbers {
 
     public int sumNumbers(TreeNode root) {
+        String sb = "";
+        ArrayList<String> result = new ArrayList<String>();
+        printTreeNode(root, sb, result);
 
-        return 0;
+        System.out.println(Arrays.toString( result.toArray()));
+
+
+        ArrayList<java.math.BigDecimal> longArrayList = new ArrayList<>();
+        for (int i = 0; i < result.size(); i++) {
+            String path = result.get(i);
+            path = path.replace("-", "");
+            Long numbers = Long.valueOf(path);
+            longArrayList.add(java.math.BigDecimal.valueOf(numbers));
+        }
+        java.math.BigDecimal finalRes= new java.math.BigDecimal(0);
+        for (java.math.BigDecimal bb :longArrayList ) {
+            finalRes = finalRes.add(bb);
+        }
+
+
+        return finalRes.intValue();
     }
 
 
-    public void depthFirstSearch(TreeNode root) {
+    public void printTreeNode(TreeNode root, String path , List<String> allPath) {
         if(root == null) {
             return;
         }
 
-        Stack<TreeNode> nodeStack = new Stack<TreeNode>();
-        nodeStack.push(root);
+        path = path + "-" + root.val;
 
-        while(!nodeStack.isEmpty()) {
-            TreeNode node = nodeStack.pop();
-            System.out.print(node.val + " ");
+        if (root.left == null && root.right == null){
+            allPath.add(path);
+            return;
+        }
 
-            if(node.right != null) {
-                nodeStack.push(node.right);
-            }
-            if(node.left != null) {
-                nodeStack.push(node.left);
-            }
+        if (root.left != null){
+            printTreeNode(root.left , path , allPath);
+        }
+
+        if (root.right != null){
+            printTreeNode(root.right , path , allPath);
         }
     }
 
     public static void main(String[] args){
         SumNumbers cache = new SumNumbers();
 
-        TreeNode root = new TreeNode(3);
-        root.right= new TreeNode(20);
+        TreeNode root = new TreeNode(4);
+        root.right= new TreeNode(0);
 
-        root.right.left = new TreeNode(6);
-        root.right.right = new TreeNode(20);
+        root.left= new TreeNode(9);
+
+        root.left.left = new TreeNode(5);
+        root.left.right = new TreeNode(1);
 
 
         System.out.println(cache.sumNumbers(root));
