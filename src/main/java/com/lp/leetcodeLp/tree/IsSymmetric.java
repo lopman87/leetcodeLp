dc11075a5a868f783a297324fdc82d7cf65c7bd5
longpeng <longpeng@lattebank.com>
2020-08-19 20:01:01 +0800

    sav

---
+++ src/main/java/com/lp/leetcodeLp/tree/IsSymmetric.java
@@ -0,0 +1,53 @@
+package com.lp.leetcodeLp.tree;
+
+
+/**
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ *  
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *  
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IsSymmetric {
+
+
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+
+    public static void main(String args[]){
+        long start = System.currentTimeMillis();
+        IsSymmetric numTrees = new IsSymmetric();
+
+        TreeNode root = new TreeNode(10);
+        root.left=new TreeNode(5);
+        root.right= new TreeNode(15);
+
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(20);
+
+
+        System.out.println(numTrees.isSymmetric(root));
+        long end = System.currentTimeMillis();
+        System.out.println("cost:"+(end - start) );
+    }
+}
