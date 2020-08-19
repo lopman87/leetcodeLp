b0384ee715447e2c9c24b14b160f049faebb76a6
lopman <longpeng@lattebank.com>
2020-08-20 00:02:29 +0800

    N

--- src/main/java/com/lp/leetcodeLp/dp/WaysToChange.java
+++ src/main/java/com/lp/leetcodeLp/dp/WaysToChange.java
@@ -28,7 +28,17 @@ public class WaysToChange {
 
 
     public int waysToChange(int n) {
-        return 0;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        int[] coins = new int[]{1,5,10,25};
+        for(int coin : coins) {
+            for(int i = coin; i <= n; i++) {
+                dp[i] = (dp[i] + dp[i - coin]) % 1000000007;
+            }
+        }
+
+        return dp[n];
+
     }
 
 
