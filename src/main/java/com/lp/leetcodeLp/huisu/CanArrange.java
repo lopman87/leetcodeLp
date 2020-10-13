ef1be88cddeb3844a768a30ca9254dadd8391c05
lopman <longpeng@lattebank.com>
2020-10-13 23:23:37 +0800

    Ewf

---
+++ src/main/java/com/lp/leetcodeLp/huisu/CanArrange.java
@@ -0,0 +1,16 @@
+package com.lp.leetcodeLp.huisu;
+
+public class CanArrange {
+
+    public boolean canArrange(int[] arr, int k) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[]arr = {1,2,3,4,5,10,6,7,8,9};
+        int k = 5;
+        CanArrange solution = new CanArrange();
+        solution.canArrange(arr, k);
+    }
+}
