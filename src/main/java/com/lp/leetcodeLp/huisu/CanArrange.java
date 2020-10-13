87db9ab4f768db1c45ce162ecec197a8e7fe8270
lopman <longpeng@lattebank.com>
2020-10-13 23:29:32 +0800

    Qd

--- src/main/java/com/lp/leetcodeLp/huisu/CanArrange.java
+++ src/main/java/com/lp/leetcodeLp/huisu/CanArrange.java
@@ -7,6 +7,15 @@ public class CanArrange {
         return false;
     }
 
+    private int other(int[] arr,int index, int k){
+        for (int i = 0; i < arr.length && i != index; i++) {
+            if ((arr[i] + arr[index])%k ==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[]arr = {1,2,3,4,5,10,6,7,8,9};
         int k = 5;
